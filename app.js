e4a386be23f1622693ed65c88d64717fd468cfd2
IrisLewy <loigdr28@gmail.com>
2026-05-01 00:32:45 +0800

    Refactor CharacterIntro component with class names
    Updated the CharacterIntro function to include class names for images and maintain structure.

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ function CharacterIntro() {
     React.createElement(
       "div",
       { className: "character-image-wrapper" },
-      React.createElement("img", { src: "Kitasan.jpg", alt: "Kitasan Black"}),
-      React.createElement("img", { src: "KitasanBlack.jpg", alt: "Kitasan Black Race"})
+      React.createElement("img", { src: "Kitasan.jpg", alt: "Kitasan Black", className: "character-image-default" }),
+      React.createElement("img", { src: "KitasanBlack.jpg", alt: "Kitasan Black Race", className: "character-image-transformed" })
     )
   );
 }
