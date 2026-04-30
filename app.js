845eaf1aff4c371956e7467e699bb57aa3620522
IrisLewy <loigdr28@gmail.com>
2026-05-01 00:43:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function CharacterIntro() {
       "div",
       { className: "character-header" },
       React.createElement("h1", null, "Kitasan Black"),
-      React.createElement("h3", null, "VA: Melissa Fahn")
+      React.createElement("h3", null, "VA: Hinaki Yano")
     ),
     React.createElement(
       "div",
@@ -14,14 +14,14 @@ function CharacterIntro() {
       React.createElement(
         "p",
         null,
-        "A new season of Elation begins, featuring the limited-time return of the invincible player \"Silver Wolf LV.999\"! She's conquered the universe, unlocked every cosmic achievement... What's left to play?"
+        "Ever bright and cheerful, she loves festivals more than anyone else. Her innate altruism makes her unable to ignore a person in need, and she's always ready to lend an ear. Her bottomless kindness has earned her the admiration of those around her, but when she gets too excited, her words take on a certain festival flair. With a smile as bright as the sun, she continues to run towards the dazzling horizon, hoping that she will discover her true dream along the way."
       )
     ),
     React.createElement(
       "div",
       { className: "character-quotes" },
-      React.createElement("p", null, "\"Reach the peak of 'Elation'? Boring... Since I've got the cartridge now, I make the rules~\""),
-      React.createElement("p", null, "\"LV.999 mode, my final form, first time seeing it, right? Let's hit up a high-difficulty Raid. I'll carry you. One-button clear.\"")
+      React.createElement("p", null, "\"I'll give it a go! You'll never know unless you try, right?\""),
+      React.createElement("p", null, "Festival Enthusiast, Helping Others, Training & Running")
     ),
     React.createElement(
       "div",
