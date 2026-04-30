9ffa84e2178e0f4c27cc96c1a66a9d3227b870f9
IrisLewy <loigdr28@gmail.com>
2026-05-01 00:14:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 function CharacterIntro() {
-  return (
-    <div className="character-card">
-      <div className="character-header">
-        <h1>Kitasan Black</h1>
-        <h3>VA: Hinaki Yano</h3>
-      </div>
-      <div className="character-description">
-        <p>
-          Ever bright and cheerful, she loves festivals more than anyone else. Her innate altruism makes her unable to ignore a person in need, and she's always ready to lend an ear. Her bottomless kindness has earned her the admiration of those around her, but when she gets too excited, her words take on a certain festival flair. With a smile as bright as the sun, she continues to run towards the dazzling horizon, hoping that she will discover her true dream along the way.
-        </p>
-      </div>
-      <div className="character-quotes">
-        <p>"I'll give it a go! You'll never know unless you try, right?"</p>
-        <p>"Harikitte Ikou!"</p>
-      </div>
-
-      {/* Image wrapper with two states */}
-      <div className="character-image-wrapper">
-        <img src="images/Kitasan.jpg" alt="Kitasan Black" className="character-image-default" />
-        <img src="images/KitasanBlack.jpg" alt="Kitasan Black Race" className="character-image-transformed" />
-      </div>
-    </div>
+  return React.createElement(
+    "div",
+    { className: "character-card" },
+    React.createElement(
+      "div",
+      { className: "character-header" },
+      React.createElement("h1", null, "Kitasan Black"),
+      React.createElement("h3", null, "VA: Melissa Fahn")
+    ),
+    React.createElement(
+      "div",
+      { className: "character-description" },
+      React.createElement(
+        "p",
+        null,
+        "A new season of Elation begins, featuring the limited-time return of the invincible player \"Silver Wolf LV.999\"! She's conquered the universe, unlocked every cosmic achievement... What's left to play?"
+      )
+    ),
+    React.createElement(
+      "div",
+      { className: "character-quotes" },
+      React.createElement("p", null, "\"Reach the peak of 'Elation'? Boring... Since I've got the cartridge now, I make the rules~\""),
+      React.createElement("p", null, "\"LV.999 mode, my final form, first time seeing it, right? Let's hit up a high-difficulty Raid. I'll carry you. One-button clear.\"")
+    ),
+    React.createElement(
+      "div",
+      { className: "character-image-wrapper" },
+      React.createElement("img", { src: "images/Kitasan.jpg", alt: "Silver Wolf", className: "character-image-default" }),
+      React.createElement("img", { src: "images/KitasanBlack.jpg", alt: "Silver Wolf Transformed", className: "character-image-transformed" })
+    )
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CharacterIntro />);
+root.render(React.createElement(CharacterIntro));
